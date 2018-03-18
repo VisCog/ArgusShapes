68a5f9314bd6c8ca465d3ff268fdeada49b6407f
Michael Beyeler <mbeyeler@uw.edu>
2018-03-17 22:53:40 -0700

    update plot crossval props

---
+++ scripts/fit_all_hires.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python crossval-hires-swarm.py E2 51-009 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py F2 51-009 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py E2 TB --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py F2 TB --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py E2 12-005 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py F2 12-005 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py E2 52-001 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py F2 52-001 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py C2 TB --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py C2 12-005 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py C2 52-001 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
+python crossval-hires-swarm.py C2 51-009 --w_scale=20 --w_rot=0 --w_dice=80 --n_folds=1 --avg_img
