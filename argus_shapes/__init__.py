cdd29e2bc710afa2dc520f0469e57bdf6348ce06
Michael Beyeler <mbeyeler@uw.edu>
2019-04-11 14:56:54 -0700

    require scikit-image 0.13

--- argus_shapes/__init__.py
+++ argus_shapes/__init__.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
-from .argus_shapes import *  # noqa
 
+import pkg_resources
+pkg_resources.require("scikit-image==0.13.*")
+
+from .argus_shapes import *  # noqa
 from . import models
 from . import model_selection
 from . import imgproc
